5882ee92cadf0dded17bb6e3eff758c562fc165d
ezg <emanuel.zgraggen@gmail.com>
2016-02-24 15:38:15 -0500

    updated nuget packages

--- PanoramicDataWin8/App.xaml.cs
+++ PanoramicDataWin8/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.ApplicationInsights;
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
 
@@ -30,8 +31,13 @@ namespace PanoramicDataWin8
         /// </summary>
         public App()
         {
+            WindowsAppInitializer.InitializeAsync();
+
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+
+            var tc = new TelemetryClient();
+            tc.TrackEvent("VizdomStarted");
         }
 
         /// <summary>
