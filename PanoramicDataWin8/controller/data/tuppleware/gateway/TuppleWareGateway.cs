b554e5d53900e93581c786bbc88f5b46b1b14dab
ezg <emanuel.zgraggen@gmail.com>
2016-03-29 15:21:44 -0400

    update

--- PanoramicDataWin8/controller/data/tuppleware/gateway/TuppleWareGateway.cs
+++ PanoramicDataWin8/controller/data/tuppleware/gateway/TuppleWareGateway.cs
@@ -5,64 +5,74 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.UI.Xaml.Controls;
 using Newtonsoft.Json.Linq;
 using PanoramicDataWin8.controller.input;
 using PanoramicDataWin8.controller.view;
 using PanoramicDataWin8.model.data;
 using PanoramicDataWin8.model.data.tuppleware;
+using PanoramicDataWin8.view.common;
 
 namespace PanoramicDataWin8.controller.data.tuppleware.gateway
 {
 
     public class TuppleWareGateway
     {
-        public static async Task<string> Request(string endPoint, JObject data)
+        public static async Task<string> Request(JObject data)
         {
-            try
+            while (true)
             {
-                if (MainViewController.Instance.MainModel.Verbose)
+                try
                 {
-                    Debug.WriteLine(data.ToString());
-                }
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-                var httpClient = new HttpClient();
-                httpClient.Timeout = TimeSpan.FromMinutes(5);
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var httpResponseMessage = await httpClient.PostAsync(endPoint, new StringContent(
-                    data.ToString(),
-                    Encoding.UTF8,
-                    "application/json"));
-                if (MainViewController.Instance.MainModel.Verbose)
-                {
-                    Debug.WriteLine("TuppleWare Roundtrip Time: " + sw.ElapsedMilliseconds);
-                }
-                sw.Restart();
+                    if (MainViewController.Instance.MainModel.Verbose)
+                    {
+                        Debug.WriteLine(data.ToString());
+                    }
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    var httpClient = new HttpClient();
+                    httpClient.Timeout = TimeSpan.FromMinutes(5);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var httpResponseMessage = await httpClient.PostAsync(MainViewController.Instance.MainModel.Ip, new StringContent(
+                        data.ToString(),
+                        Encoding.UTF8,
+                        "application/json"));
+                    if (MainViewController.Instance.MainModel.Verbose)
+                    {
+                        Debug.WriteLine("TuppleWare Roundtrip Time: " + sw.ElapsedMilliseconds);
+                    }
+                    sw.Restart();
 
-                var stringContent = await httpResponseMessage.Content.ReadAsStringAsync();
-                if (MainViewController.Instance.MainModel.Verbose)
-                {
-                    Debug.WriteLine("TuppleWare Read Content Time: " + sw.ElapsedMilliseconds);
-                }
-                sw.Restart();
+                    var stringContent = await httpResponseMessage.Content.ReadAsStringAsync();
+                    if (MainViewController.Instance.MainModel.Verbose)
+                    {
+                        Debug.WriteLine("TuppleWare Read Content Time: " + sw.ElapsedMilliseconds);
+                    }
+                    sw.Restart();
 
-                return stringContent;
+                    return stringContent;
+                }
+                catch (Exception e)
+                {
+                    GatewayErrorDialog dialog = new GatewayErrorDialog()
+                    {
+                        Ip = MainViewController.Instance.MainModel.Ip,
+                        Content = e.Message
+                    };
+
+                    ContentDialogResult result = await dialog.ShowAsync();
+
+                    if (result == ContentDialogResult.Secondary)
+                    {
+                        CoreApplication.Exit();
+                    }
+                    else
+                    {
+                        MainViewController.Instance.MainModel.Ip = dialog.Ip;
+                    }
+                }
             }
-            catch (Exception)
-            {
-                ContentDialog noWifiDialog = new ContentDialog()
-                {
-                    Title = "No wifi connection",
-                    Content = "Check connection and try again",
-                    PrimaryButtonText = "Ok"
-                };
-
-                ContentDialogResult result = await noWifiDialog.ShowAsync();
-                
-                throw;
-            }
-            return "";
         }
     }
 }
