4e9b276a4e757145ee7cf267925b1b75470f16d1
ezg <emanuel.zgraggen@gmail.com>
2016-05-24 19:08:23 -0400

    update

--- backend/randomtest.py
+++ backend/randomtest.py
@@ -16,20 +16,22 @@ job = {
     "type": "visualization",
     "chunkSize": 10000,
     "aggregateDimensions": [
-      "obama"
+      "asian"
     ],
     "nrOfBins": [
       10.0,
       10.0
     ],
-    "brushes": [],
+    "brushes": [
+      "( county == 'F46107' ) or ( county == 'F46129' ) or ( county == 'F8011' ) or ( county == 'F8017' ) or ( county == 'F8061' ) or ( county == 'F8063' ) or ( county == 'F8095' ) or ( county == 'F8099' ) or ( county == 'F8115' ) or ( county == 'F8125' ) or ( county == 'F31049' ) or ( county == 'F31069' ) or ( county == 'F5005' ) or ( county == 'F5029' ) or ( county == 'F5051' ) or ( county == 'F5089' ) or ( county == 'F5105' ) or ( county == 'F5115' ) or ( county == 'F5125' ) or ( county == 'F5129' ) or ( county == 'F5141' ) or ( county == 'F5149' ) or ( county == 'F17001' ) or ( county == 'F17067' ) or ( county == 'F19087' ) or ( county == 'F19103' ) or ( county == 'F19111' ) or ( county == 'F19113' ) or ( county == 'F19115' ) or ( county == 'F19177' ) or ( county == 'F19183' ) or ( county == 'F29007' ) or ( county == 'F29019' ) or ( county == 'F29027' ) or ( county == 'F29045' ) or ( county == 'F29051' ) or ( county == 'F29067' ) or ( county == 'F29105' ) or ( county == 'F29111' ) or ( county == 'F29125' ) or ( county == 'F29127' ) or ( county == 'F29131' ) or ( county == 'F29137' ) or ( county == 'F29151' ) or ( county == 'F29153' ) or ( county == 'F29169' ) or ( county == 'F29173' ) or ( county == 'F29205' ) or ( county == 'F29215' ) or ( county == 'F29229' )"
+    ],
     "dimensionAggregateFunctions": [
       "None",
       "Count"
     ],
     "dimensions": [
-      "obama",
-      "obama"
+      "asian",
+      "asian"
     ]
   }
 }
